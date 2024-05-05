b92eaa40d3dc8cea6d30f20f245af9fce49be72c
63n0 <98720107+63n0@users.noreply.github.com>
2024-05-05 13:04:52 +0900

    Sessionにupdate関連の処理を追加

--- arproxy-macfilter-agent/src/repositories/session.rs
+++ arproxy-macfilter-agent/src/repositories/session.rs
@@ -2,6 +2,8 @@ use std::sync::{Arc, RwLock};
 
 use rand::Rng;
 
+use super::RepositoryError;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct SessionId([u8; 32]);
 
@@ -28,9 +30,10 @@ impl<D> Session<D> {
 }
 
 pub trait SessionRepository<D>: Clone + std::marker::Send + std::marker::Sync + 'static {
-    fn add(&self, data: D) -> SessionId;
-    fn delete(&self, sessid: SessionId);
+    fn add(&self, data: D) -> Session<D>;
     fn get(&self, sessid: SessionId) -> Option<Session<D>>;
+    fn update(&self, sessid:SessionId, data:D) -> Result<Session<D>, RepositoryError>;
+    fn delete(&self, sessid: SessionId);
     fn clear(&self);
 }
 
@@ -54,21 +57,12 @@ impl<D> SessionRepository<D> for SingletonSessionRepositoryForMemory<D>
 where
     D: SessionData,
 {
-    fn add(&self, data: D) -> SessionId {
+    fn add(&self, data: D) -> Session<D> {
         let sess = Session::new(data);
         let id = sess.id;
         let mut store = self.store.write().unwrap();
-        *store = Some(sess);
-        id
-    }
-
-    fn delete(&self, sessid: SessionId) {
-        let mut store = self.store.write().unwrap();
-        if let Some(sess) = store.clone() {
-            if sess.id == sessid {
-                *store = None;
-            }
-        }
+        *store = Some(sess.clone());
+        sess
     }
 
     fn get(&self, sessid: SessionId) -> Option<Session<D>> {
@@ -81,14 +75,38 @@ where
         None
     }
 
+    fn update(&self, sessid:SessionId, data:D) -> Result<Session<D>, RepositoryError> {
+        let mut store = self.store.write().unwrap();
+        if let Some(mut sess) = store.clone() {
+            if sess.id == sessid {
+                sess.data = data;
+                return Ok(sess);
+            } 
+        }
+        Err(RepositoryError::NotFound)
+    }
+
+    fn delete(&self, sessid: SessionId) {
+        let mut store = self.store.write().unwrap();
+        if let Some(sess) = store.clone() {
+            if sess.id == sessid {
+                *store = None;
+            }
+        }
+    }
+
     fn clear(&self) {
         let mut store = self.store.write().unwrap();
         *store = None;
     }
+    
+
 }
 
 #[cfg(test)]
 mod test {
+    use crate::repositories::RepositoryError;
+
     use super::{SessionData, SessionRepository, SingletonSessionRepositoryForMemory};
 
     #[derive(Debug, Clone, PartialEq, Eq)]
@@ -107,7 +125,7 @@ mod test {
         let sess_data = SessionDataTest::new(35);
         let sess_repo = SingletonSessionRepositoryForMemory::<SessionDataTest>::new();
         // get()
-        let sess1_id = sess_repo.add(sess_data.clone());
+        let sess1_id = sess_repo.add(sess_data.clone()).id;
         assert_eq!(sess_repo.get(sess1_id).unwrap().data.value, 35);
         // delete()
         sess_repo.delete(sess1_id);
@@ -115,16 +133,21 @@ mod test {
         assert_eq!(sess_repo.get(sess1_id), None);
         // clear()
         let sess_data = SessionDataTest::new(45);
-        let _sess2_id = sess_repo.add(sess_data);
+        sess_repo.add(sess_data);
         sess_repo.clear();
         sess_repo.clear(); // call twice
         assert_eq!(sess_repo.get(sess1_id), None);
         // add() singleton check
         let sess_data = SessionDataTest::new(55);
-        let sess3_id = sess_repo.add(sess_data);
+        let sess3_id = sess_repo.add(sess_data).id;
         let sess_data = SessionDataTest::new(65);
-        let sess4_id = sess_repo.add(sess_data);
+        let sess4_id = sess_repo.add(sess_data).id;
         assert_eq!(sess_repo.get(sess3_id), None);
         assert_eq!(sess_repo.get(sess4_id).unwrap().data.value, 65);
+        // update()
+        let result = sess_repo.update(sess4_id, SessionDataTest::new(12));
+        assert_eq!(result.unwrap().data.value, 12);
+        let result = sess_repo.update(sess1_id, SessionDataTest::new(12));
+        assert_eq!(result, Err(RepositoryError::NotFound));
     }
 }
