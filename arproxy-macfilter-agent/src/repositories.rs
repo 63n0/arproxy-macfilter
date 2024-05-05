8963673712400fa16e2a650eee342053c269abeb
63n0 <98720107+63n0@users.noreply.github.com>
2024-05-05 13:04:52 +0900

    Sessionにupdate関連の処理を追加

--- arproxy-macfilter-agent/src/repositories.rs
+++ arproxy-macfilter-agent/src/repositories.rs
@@ -3,8 +3,10 @@ pub mod arplog;
 pub mod config;
 pub mod session;
 
-#[derive(Debug, thiserror::Error)]
+#[derive(Debug, PartialEq, Eq,thiserror::Error)]
 pub enum RepositoryError {
     #[error("Failed to get resource")]
     SyncFailed,
+    #[error("Resource not found")]
+    NotFound,
 }
